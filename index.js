89b73f013c03d6a39d638199a292a914463847f9
OnedinKenedi <frimmelj@gmail.com>
2020-12-15 19:54:35 +0100

    add new task / print error message

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ if (Object.keys(args).length < 2) {
     console.log(manual);
   };
 
-  // listing tasks
+  // listing tasks -l
 
 if (args.l) {
     const content = fs.readFileSync('tasks.txt', 'utf-8');
@@ -23,7 +23,22 @@ if (args.l) {
             if (todo[0] === '/') {
             todo = todo.slice(1);
             }
-        console.log(`${i + 1}. ${todo}`);
+        console.log(`${i + 1} - ${todo}`);
     });
     }
-}
+};
+
+function addTask(todo) {
+	const content = fs.readFileSync('tasks.txt', 'utf-8');
+	if (content.length === 0) {
+		fs.appendFileSync('tasks.txt', todo);
+	} else {
+		fs.appendFileSync('tasks.txt', '\n' + todo);
+	}
+};
+
+if (args.a) {
+    console.log('Nem lehetséges új feladat hozzáadása: nincs megadva a feladat!');
+} else if (typeof args.a === 'string') {
+    addTask(args.a);
+};;
